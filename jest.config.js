e2b3069e1fa89a4e0388f17bbc1647a4158f12b1
armonkahil <armonkahil@gmail.com>
2021-10-19 21:13:13 -0400

    another update

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
+  collectCoverageFrom: [
+        './src/**/*.ts'
+    ],
   globals: {
     "ts-jest": {
       diagnostics: true
     }
   },
   coverageReporters: [
-    "lcov"
+    "lcov",
   ]
 }
